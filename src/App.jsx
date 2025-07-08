319da25c82312e8e806be465a90b5e536ff05f8d
VansitaGajjar <vansitaygajjar@gmail.com>
2025-07-08 15:50:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Testing CI/CD Pipeline.... 
+      </p>
+    </>
+  )
+}
+
+export default App
