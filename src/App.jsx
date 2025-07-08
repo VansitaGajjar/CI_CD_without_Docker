352fa67420634d3872e53a6b46c70d7034608909
VansitaGajjar <vansitaygajjar@gmail.com>
2025-07-08 18:04:22 +0530

    Implemented Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import './App.css'
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
-      <p className="read-the-docs">
-        Testing CI/CD Pipeline.... 
-      </p>
+      <div className="flex justify-center items-center bg-black min-h-screen min-w-screen gap-4">
+        <Card
+        src="https://picsum.photos/id/1018/400/250"
+        title="Exploring the Mountains"
+        desc="Join us on a journey through breathtaking mountain landscapes and thrilling adventures."
+        link="https://www.britannica.com/story/exploring-7-of-earths-great-mountain-ranges"
+      />
+      <Card
+        src="https://picsum.photos/id/1025/400/250"
+        title="Into the Wild"
+        desc="Discover the beauty of untamed nature and wildlife in this immersive experience."
+        link="https://www.junglesafari.in/"
+      />
+      <Card
+        src="https://picsum.photos/id/1035/400/250"
+        title="City Lights at Night"
+        desc="A visual diary of mesmerizing cityscapes illuminated by night lights."
+        link="https://www.pexels.com/search/city%20lights/"
+      />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CI_CD_without_Docker/',
-  plugins: [react()],
-})
+  base: "/CI_CD_without_Docker/",
+  plugins: [react(), tailwindcss()],
+});
